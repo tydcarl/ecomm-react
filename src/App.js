9a8ec6675c8b6d5f965f5109d10a5a803af7c021
Tyler <tydcarl@gmail.com>
2025-09-10 14:58:50 -0700

    better

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import Nav  from './components/Nav';
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-    <Nav />
+      <Nav />
     </div>
   );
 }
