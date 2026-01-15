f5209ec3554d3b239b115b1004737f2794f510c9
Tyler <tydcarl@gmail.com>
2026-01-15 10:47:05 -0800

    back at it

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Book from "./ui/Book";
+import { books } from "../data";
 
 const Featured = () => {
+  console.log(books);
+  console.log();
   return (
     <section id="features">
       <div className="container">
@@ -10,10 +13,12 @@ const Featured = () => {
             Featured <span className="purple">Books</span>
           </h2>
           <div className="books">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            {books
+            .filter((book) => book.rating === 5)
+            .slice(0, 4)
+            .map((book) =>  (
+            <Book book={book}/>
+            ))}
           </div>
         </div>
       </div>
