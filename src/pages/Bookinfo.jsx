7ceac0fcfed8a7f35df0f7fcb082bde29a54330a
Tyler <tydcarl@gmail.com>
2026-02-06 19:42:11 -0800

    huh

--- src/pages/Bookinfo.jsx
+++ src/pages/Bookinfo.jsx
@@ -23,15 +23,45 @@ const BookInfo = ({ books }) => {
                 <img src="" alt="" className="book__selected--ig" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">Crack the Coding Interview</h2>
+                <h2 className="book__selected--title">
+                  Crack the Coding Interview
+                </h2>
                 <Rating rating="4.5" />
                 <div className="book__selected--price">
-                    <Price originalPrice={50} salePrice={null}/>
-
+                  <Price originalPrice={50} salePrice={null} />
                 </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">Summary</div>
+                  <p className="book__summary__para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae libero cumque exercitationem quia atque? Eum,
+                    doloremque. Autem accusamus, quo optio tenetur, id nihil
+                    perferendis nam reiciendis asperiores in fugiat
+                    exercitationem!
+                  </p>
+                  <p className="book__summary__para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae libero cumque exercitationem quia atque? Eum,
+                    doloremque. Autem accusamus, quo optio tenetur, id nihil
+                    perferendis nam reiciendis asperiores in fugiat
+                    exercitationem!
+                  </p>
+                </div>
+                <button className="btn">
+                    Add to Cart
+                </button>
               </div>
             </div>
           </div>
+        </div>
+        <div className="bboks__container">
+            <div className="row">
+                <div className="books__selected--top">
+                    <div className="book__selected--title--top">
+                        Recommended Books
+                    </div>
+                </div>
+            </div>
         </div>
       </main>
     </div>
